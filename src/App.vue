d3f5e6d150900c34ca5d58b5b931f113ba49f69e
bonvi83 <bonvi83@gmail.com>
2024-04-13 11:10:46 +0200

    stampa in pagina

--- src/App.vue
+++ src/App.vue
@@ -5,18 +5,34 @@ export default {
   data() {
     return {
       title: "Boolpress Frontend",
+      projects: [],
     };
   },
 
   created() {
-    axios.get("http://127.0.0.1:8000/").then((response) => {});
+    axios.get("http://127.0.0.1:8000/api/projects").then((response) => {});
+    // console.log(response.data.data);
+    this.projects = response.data.data;
   },
 };
 </script>
 
 <template>
-  <div class="container mt 5">
+  <div class="container mt-5">
     <h1>{{ title }}</h1>
+    <div v-for="project in projects">
+      <ul>
+        <li>
+          <strong>Title:</strong>
+          {{ project.title }}
+        </li>
+        <li>
+          <strong>Content:</strong>
+          {{ project.content }}
+        </li>
+      </ul>
+      <hr />
+    </div>
   </div>
 </template>
 
